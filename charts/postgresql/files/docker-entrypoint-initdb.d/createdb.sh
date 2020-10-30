41ecbb9896adb88ae3e0dfcdc7f33308922444fb
hsoltani <hsoltani@users.noreply.github.com>
2020-10-30 11:18:18 +0330

    Update createdb.sh

--- charts/postgresql/files/docker-entrypoint-initdb.d/createdb.sh
+++ charts/postgresql/files/docker-entrypoint-initdb.d/createdb.sh
@@ -5,16 +5,16 @@
 
 #support for OS
 case $OSTYPE in
-darwin*) psql postgres -d fabricexplorer -U hppoc -W password -f ./explorerpg.sql ;
-psql postgres -d fabricexplorer -U hppoc -W password -f ./updatepg.sql ;;
+darwin*) psql postgres -d fabricexplorer -U hppoc -f ./explorerpg.sql ;
+psql postgres -d fabricexplorer -U hppoc -f ./updatepg.sql ;;
 linux*)
 if [ $(id -un) = 'postgres' ]; then
   PSQL="psql"
 else
   PSQL="sudo -u postgres psql"
 fi;
-${PSQL} -d fabricexplorer -U hppoc -W password -f ./explorerpg.sql ;
-${PSQL} -d fabricexplorer -U hppoc -W password -f ./updatepg.sql ;;
+${PSQL} -d fabricexplorer -U hppoc -f ./explorerpg.sql ;
+${PSQL} -d fabricexplorer -U hppoc -f ./updatepg.sql ;;
 esac
 
 
